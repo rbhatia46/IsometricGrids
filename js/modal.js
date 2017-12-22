2503c49bcae15456980897b7148bb548245e5fd1
sunilsj99 <rpspjjjj@gmail.com>
2017-12-22 21:37:42 +0530

    model added

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal-trigger').leanModal();
+  $('#modal1').modal();
+  $('#modal2').modal();
+  $('#modal3').modal();
+  $('#modal4').modal();
+  $('#modal5').modal();
+  $('#modal6').modal();
+  $('#modal7').modal();
+  $('#modal8').modal();
+  $('#modal9').modal();
+  $('#modal10').modal();
+  $('#modal11').modal();
+  $('#modal12').modal();
+
 });
